e324f0674c890c4f39021a9b1b0b1511ca0f2a0e
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2023-12-17 22:24:32 +0100

    route fix 2

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import "./styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Forside">
       <App></App>
     </BrowserRouter>
   </React.StrictMode>
