e610541b159361ff3c2c38ec96e63542fb0b8b0a
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2023-12-17 22:31:48 +0100

    12

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import "./styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Forside">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App></App>
     </BrowserRouter>
   </React.StrictMode>
