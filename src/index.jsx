2f506abcde36e9ba24419dd2d33ea5f31496ea42
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2023-12-18 10:20:57 +0100

    Structure change

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import NavbarComponent from "./components/NavbarComponent";
+import { Route, Routes } from "react-router-dom";
+import Forside from "./pages/Forside";
+import OmKoerestolsrugby from "./pages/OmKoerestolsrugby";
+import OmBurnouts from "./pages/OmBurnouts";
+import Nyheder from "./pages/Nyheder";
+import Kontakt from "./pages/Kontakt";
+import Kalender from "./pages/Kalender";
+import FooterComponent from "./components/FooterComponent";
+import { HelmetProvider } from "react-helmet-async";
 import "./styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App></App>
-    </HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HelmetProvider>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Forside />}></Route>
+          <Route
+            path="/OmKoerestolsrugby"
+            element={<OmKoerestolsrugby />}
+          ></Route>
+          <Route path="/OmBurnouts" element={<OmBurnouts />}></Route>
+          <Route path="/Nyheder" element={<Nyheder />}></Route>
+          <Route path="/Kalender" element={<Kalender />}></Route>
+          <Route path="/Kontakt" element={<Kontakt />}></Route>
+        </Routes>
+        <FooterComponent />
+      </HelmetProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
